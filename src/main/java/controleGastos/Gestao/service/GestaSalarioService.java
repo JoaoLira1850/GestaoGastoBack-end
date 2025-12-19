0119b93df99b82df1aee45418cfdfa69f0d8bb36
João Vitor <joaovitortba@gmail.com>
2025-12-18 22:06:30 -0300

    Colocando Buscar salario, deletar salario, buscar por id, alterar por id

--- src/main/java/controleGastos/Gestao/service/GestaSalarioService.java
+++ src/main/java/controleGastos/Gestao/service/GestaSalarioService.java
@@ -1,6 +1,8 @@
 package controleGastos.Gestao.service;
 
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,66 @@ public class GestaSalarioService {
         
         return listaSalario.stream().map(GestaSalarioDTO :: new ).toList();
         
+    }
+
+
+    public GestaSalarioDTO criarSalario (GestaSalarioDTO gestaSalarioDTO){
+
+        GestaoSalarioEntity gestaoSalarioEntity = new GestaoSalarioEntity(gestaSalarioDTO);
+
+        GestaoSalarioEntity criarSalario =  gestaoSalarioRepository.save(gestaoSalarioEntity);
+
+        return new GestaSalarioDTO(criarSalario); 
+        
+
+
+
+    }
+
+
+    public Optional<GestaSalarioDTO> deletarSalario( Long id){
+
+        Optional<GestaoSalarioEntity> salarioBuscado = gestaoSalarioRepository.findById(id);
+
+        if(salarioBuscado.isPresent()){
+            GestaoSalarioEntity salario = salarioBuscado.get();
+
+            gestaoSalarioRepository.delete(salario);
+
+            
+
+            return Optional.of( new GestaSalarioDTO(salarioBuscado.get()));
+            
+
+        }else{
+            return  Optional.empty();
+        }
+    }
+
+
+    public Optional<GestaSalarioDTO> alterarSalario( GestaSalarioDTO gestaSalarioDTO, Long id){
+
+
+        Optional<GestaoSalarioEntity> salarioBuscado = gestaoSalarioRepository.findById(id);
+
+        if(salarioBuscado.isPresent()){
+            GestaoSalarioEntity salarioAlterar = salarioBuscado.get();
+
+            salarioAlterar.setData(gestaSalarioDTO.getData());
+            salarioAlterar.setFuncao(gestaSalarioDTO.getFuncao());
+            salarioAlterar.setSalario(gestaSalarioDTO.getSalario());
+            
+            gestaoSalarioRepository.save(salarioAlterar);
+
+            GestaSalarioDTO salarioDTO = new GestaSalarioDTO(salarioAlterar);
+
+            return Optional.of(salarioDTO);
+
+
+        }else{
+            return Optional.empty();
+        }
+
     }
 
 }
